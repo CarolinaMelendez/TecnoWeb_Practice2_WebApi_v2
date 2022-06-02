15c96862471d14b5e69f0aab7ccb2ab30adb6099
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2022-06-02 08:42:08 -0400

    P2-001 created userManager and basic code

---
+++ Logic/UserManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Logic
+{
+    public class UserManager
+    {
+        public List<User> Users { get; set }
+        public UserManager()
+        {
+            Users =  new List<User>()
+            {
+                new User(){ Ci = 10 , Name = "Carolina"},
+                new User(){ Ci = 11 , Name = "Franco"},
+                new User(){ Ci = 12 , Name = "Melany"}
+            };
+        }
+        public List<User> GetUsers()
+        {
+            return Users;
+        }
+      
+        public User PostUse(User user)
+        {
+            Users.Add(user);
+            return user;
+        }
+        
+        public User PutUser(User user)
+        {
+            // suponiendo que solo quiera cambiar el nombre
+            Users.Find(u => u.Ci == user.Ci).Name = user.Name;
+            return user;
+        }
+       
+        public User DeleteUser(User user)
+        {
+            Users.Remove(user);
+            return user;
+        }
+    }
+}
