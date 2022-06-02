f901529248586297fc5b87dc778263e5d9d823d2
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2022-06-02 08:22:07 -0400

    P2-001 created controller users file

---
+++ Practice_2_v2/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practice_2_v2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult PostUse()
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult PutUser()
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteUser()
+        {
+            return Ok();
+        }
+    }
+}
